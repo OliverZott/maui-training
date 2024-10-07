3d42f31c34acfd9ca85bbc32e28fd7bf8c65d714
Oliver Zott <zott_oliver@web.de>
2024-10-07 19:32:49 +0200

    Add project files.

---
+++ maui-training/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace maui_training;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ maui-training/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,8 @@
+﻿using Foundation;
+
+namespace maui_training;
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
