aa01a81c0d23f80a41d7007c6a7ccf770b0d25af
Oliver Zott <zott_oliver@web.de>
2024-10-09 12:10:09 +0200

    Phone number translation example

--- maui-training/MainPage.xaml.cs
+++ maui-training/MainPage.xaml.cs
@@ -1,24 +1,51 @@
 ﻿namespace maui_training;
 
 public partial class MainPage : ContentPage
-{
-    int count = 0;
+    {
+    public string? translatedNumber;
 
     public MainPage()
-    {
+        {
         InitializeComponent();
-    }
+        }
 
-    private void OnCounterClicked(object sender, EventArgs e)
-    {
-        count++;
-
-        if (count == 1)
-            CounterBtn.Text = $"Clicked {count} time";
+    private void OnTranslate(object sender, EventArgs e)
+        {
+        string enterdNumber = PhoneNumberText.Text;
+        string? translatedNumber = PhonewordTranslator.ToNumber(enterdNumber);
+        if (!string.IsNullOrEmpty(translatedNumber))
+            {
+            CallButton.IsEnabled = true;
+            CallButton.Text = $"Call {translatedNumber}";
+            }
         else
-            CounterBtn.Text = $"Clicked {count} times";
+            {
+            CallButton.IsEnabled = false;
+            CallButton.Text = "Call";
+            }
+        }
 
-        SemanticScreenReader.Announce(CounterBtn.Text);
+    async void OnCall(object sender, EventArgs e)
+        {
+        if (await DisplayAlert(
+          "Dial a number", $"Would you like to call {translatedNumber}?", "Yes", "No"))
+            {
+            try
+                {
+                if (PhoneDialer.Default.IsSupported)
+                    {
+                    PhoneDialer.Default.Open(translatedNumber);
+                    }
+                }
+            catch (ArgumentNullException)
+                {
+                await DisplayAlert("Unable to dial", "Phone number not valid.", "OK");
+                }
+            catch  (Exception)
+                {
+                await DisplayAlert("Unable to dial", "Phone dialing failed.", "OK");
+                }
+            }
+        }
     }
-}
 
---
+++ maui-training/PhonewordTranslator.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+
+namespace maui_training;
+
+public static class PhonewordTranslator
+  {
+
+  public static string? ToNumber(string raw)
+    {
+    if (string.IsNullOrWhiteSpace(raw))
+      return null;
+
+      raw = raw.ToUpperInvariant();
+
+    var newNumber = new StringBuilder();      // debug here what happens if just stringbuilder?? what to return?
+
+    foreach (var c in raw)
+      {
+      if ("-0123456789".Contains(c))
+        {
+        newNumber.Append(c);
+        }
+      else
+        {
+        var translatedNumber = TranslateToNumber(c);
+        if (translatedNumber != null)
+          {
+          newNumber.Append(translatedNumber);
+          }
+       else return null;
+        }
+      }
+    return newNumber.ToString();
+    }
+
+
+  static readonly string[] digits =
+    [
+      "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"
+    ];
+
+
+
+  private static int? TranslateToNumber(char c)
+    {
+    for (int i = 0; i < digits.Length; i++)
+      {
+      if (digits[i].Contains(c)) return 2 + i;
+      }
+    return null;
+    }
+  }
