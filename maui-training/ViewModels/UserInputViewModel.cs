70c2679d1a14fd2ee1d5e832388b38c15d82cf7a
Oliver Zott <zott_oliver@web.de>
2024-10-17 12:48:38 +0200

    #1 Logic moved to ViewModel, simple input validation added

---
+++ maui-training/ViewModels/UserInputViewModel.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using maui_training.Models;
+
+namespace maui_training.ViewModels;
+
+internal partial class UserInputViewModel : ObservableObject
+{
+    private string? firstName;
+    private string? lastName;
+    private string? age;
+    private ObservableCollection<User> users;
+
+    public string FirstName
+    {
+        get => firstName;
+        set => SetProperty(ref firstName, value);
+    }
+    public string LastName
+    {
+        get => lastName;
+        set => SetProperty(ref lastName, value);
+    }
+    public string Age
+    {
+        get => age;
+        set => SetProperty(ref age, value);
+    }
+    public ObservableCollection<User> Users
+    {
+        get => users;
+        set => SetProperty(ref users, value);
+    }
+
+
+
+    public Command SaveUserCommand { get; }
+
+    public UserInputViewModel()
+    {
+        InitializeUsers();
+        SaveUserCommand = new Command(async () => await SaveUser());
+    }
+
+    public async void InitializeUsers()
+    {
+        Users = new ObservableCollection<User>(await App.SqliteDatabaseService.GetAllUsersAsync());
+    }
+
+    private async Task SaveUser()
+    {
+        if (string.IsNullOrWhiteSpace(FirstName) ||
+            string.IsNullOrWhiteSpace(LastName) ||
+            string.IsNullOrWhiteSpace(Age))
+        {
+            // could be removed as soon as condition "canSaveUser" works in command instantiation
+            await App.Current.MainPage.DisplayAlert("Error", "All fields are required.", "OK");
+
+            return;
+        }
+
+        try
+        {
+            var user = new User
+            {
+                FirstName = FirstName,
+                LastName = LastName,
+                Age = int.Parse(Age)
+            };
+
+            await App.SqliteDatabaseService.CreateUserAsync(user);
+            Users.Add(user);
+            FirstName = LastName = Age = string.Empty;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+    }
+
+    private bool CanSaveUser()
+    {
+        var canSave = !string.IsNullOrWhiteSpace(FirstName) ||
+                      !string.IsNullOrWhiteSpace(LastName) ||
+                      !string.IsNullOrWhiteSpace(Age);
+        return canSave;
+    }
+}
--- maui-training/Views/Pages/UserInputPage.xaml.cs
+++ maui-training/Views/Pages/UserInputPage.xaml.cs
@@ -1,40 +1,18 @@
-using maui_training.Models;
+using maui_training.ViewModels;
 
 namespace maui_training.Views.Pages;
 
-
-// TODO: remove from code behind and use UserViewModel as DTO ?!?
-// TODO: inject databaseservice and use DI
 public partial class UserInputPage : ContentPage
 {
     public UserInputPage()
     {
         InitializeComponent();
+        BindingContext = new UserInputViewModel();
     }
 
-    protected override async void OnAppearing()
+    protected override void OnAppearing()
     {
         base.OnAppearing();
-        PeopleListView.ItemsSource = await App.SqliteDatabaseService.GetAllUsersAsync();
-    }
-
-    public async void OnSaveButtonClicked(object sender, EventArgs e)
-    {
-        var user = new User
-        {
-            FirstName = FirstNameEntry.Text,
-            LastName = LastNameEntry.Text,
-            Age = int.Parse(AgeEntry.Text)
-        };
-
-        // TODO: specific convention App. ... instead of instantiation/DI ???
-        await App.SqliteDatabaseService.CreateUserAsync(user);
-
-        // Clear input form
-        FirstNameEntry.Text = LastNameEntry.Text = AgeEntry.Text = string.Empty;
-
-        // Refresh View
-        PeopleListView.ItemsSource = await App.SqliteDatabaseService.GetAllUsersAsync();
-
+        ((UserInputViewModel)BindingContext).InitializeUsers();
     }
 }
